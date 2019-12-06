bc14ddc28d25efbed5cf0682c46c8c204a7248da
gponsu <simpons@gmail.com>
2019-12-06 08:21:14 +0100

    chore: introduce firebase

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 const withOffline = require('next-offline')
+const path = require("path");
+
+require('dotenv').config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`)
+});
 
 const nextConfig = {
   workboxOpts: {
     importScripts: ['notifications.js']
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
   }
 }
 
