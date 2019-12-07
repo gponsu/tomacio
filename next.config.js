7d808606df4c7f96ddaa897e47b8e75ba606e86d
gponsu <simpons@gmail.com>
2019-12-07 16:14:37 +0100

    chore: introduce Sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-const withOffline = require('next-offline')
+const withSourceMaps = require('@zeit/next-source-maps');
+const withOffline = require('next-offline');
 const path = require("path");
 
 require('dotenv').config({
@@ -6,6 +7,12 @@ require('dotenv').config({
 });
 
 const nextConfig = {
+  webpack(config, options) {
+    if (!options.isServer)
+      config.resolve.alias['@sentry/node'] = '@sentry/browser';
+
+    return config;
+  },
   workboxOpts: {
     importScripts: ['notifications.js']
   },
@@ -17,8 +24,9 @@ const nextConfig = {
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    SENTRY_DSN: process.env.SENTRY_DSN
   }
 }
 
-module.exports = withOffline(nextConfig)
+module.exports = withSourceMaps(withOffline(nextConfig));
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
-import App from 'next/app'
-import Router from 'next/router'
+import React from 'react';
+import App from 'next/app';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+
 import firebase from "../utils/firebase";
 import "firebase/analytics";
 
 if (typeof window !== 'undefined')
   firebase.analytics();
 
-export default App;
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    // Workaround for https://github.com/zeit/next.js/issues/8592
+    const { err } = this.props
+    const modifiedPageProps = { ...pageProps, err }
+
+    return <Component {...modifiedPageProps} />
+  }
+}
+
+export default MyApp;
