725352de5035e60ef27cc05fab6c3a252039b357
gponsu <simpons@gmail.com>
2019-12-07 16:14:37 +0100

    chore: introduce Sentry

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import App from 'next/app'
 import Router from 'next/router'
+import * as Sentry from '@sentry/browser';
 import firebase from "../utils/firebase";
 import "firebase/analytics";
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 if (typeof window !== 'undefined')
   firebase.analytics();
